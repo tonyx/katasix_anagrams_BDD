e3d54ab04926befef2cf506f7a670d8a09c78cc1
Antonio <tonyx@yahoo.com>
2012-02-11 23:06:15 +0100

    kata

---
+++ AnagramSteps.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace AnagramsKata
+{
+    [Binding]
+    class AnagramSteps
+    {
+        private Anagrams anagrams;
+        private HashSet<String> words;
+        private HashSet<String> anagramList;
+        
+        [Given(@"I have an empty dictionary")]
+        public void GivenAnEmptyDictionary()
+        {
+            words = new HashSet<string>();
+            anagrams = new Anagrams(words);
+        }
+
+        [When(@"I ask for the anagrams of the word (.*)")]
+        public void WhenIAskForTheAnagramsOfTheWord(string word)
+        {
+            anagramList = anagrams.AnagramsOf(word);
+        }
+
+        [Then(@"the result should be an empy set of angrams")]
+        public void ThenTheResultShouldBeAnEmptySetOfAnagrams()
+        {
+            Assert.That(0, Is.EqualTo(anagramList.Count));
+        }
+
+        [Given(@"I have dictionary containing the words: (.*)")]
+        public void GivenIHaveDictionaryContainingTheWordsFoo(string word)
+        {
+            words = new HashSet<string>();
+            foreach (string singleWord in word.Split(','))
+            {
+                words.Add(singleWord.Trim());
+            }
+            anagrams = new Anagrams(words);
+        }
+
+        [When(@"I ask for the anagram of the word (.*)")]
+        public void WhenIAskForTheAnagramOfWordFoo(string word)
+        {
+            anagramList = anagrams.AnagramsOf(word);
+        }
+
+        [Then(@"the result should be a set containing the words: (.*)")]
+        public void ThenTheResultShouldBeASetContainingTheWordsFoo(string word)
+        {
+            string[] wordList = word.Split(',');
+            foreach (string singleWord in wordList)
+            {
+                Assert.That(anagramList.Contains(singleWord.Trim()));
+            }
+        }
+
+        [Then(@"the result should not contain the words: (.*)")]
+        public void ThenTheResultShouldNotContainTheWords(string word)
+        {
+            string[] wordList = word.Split(',');
+            foreach (string singleWord in wordList)
+            {
+                Assert.IsFalse(anagramList.Contains(singleWord.Trim()));
+            }
+        }
+
+
+       
+    }
+}
---
+++ Anagrams.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AnagramsKata
+{       
+    public class Anagrams
+    {
+        private HashSet<string> _words;
+
+        public Anagrams(HashSet<string> words)
+        {
+            _words = words;
+        }
+
+        public HashSet<string> AnagramsOf(string aWord)
+        {
+            HashSet<string> toReturn = new HashSet<string>();
+            foreach (string word in _words)
+            {
+                if (AreAnagrams(aWord, word))
+                    toReturn.Add(word);
+            }
+            return toReturn;
+        }
+
+        private bool AreAnagrams(string word1, string word2)
+        {
+            Dictionary<char,int> occurWord1 = new Dictionary<char, int>();
+            Dictionary<char,int> occurWord2 = new Dictionary<char, int>();
+
+            MapOccurrences(word1, occurWord1);
+            MapOccurrences(word2, occurWord2);
+
+            if (!SameKeys(occurWord2, occurWord1)) return false;
+            if (!SameOccurrences(occurWord2, occurWord1)) return false;
+
+            return true;
+        }
+
+        private static bool SameOccurrences(Dictionary<char, int> occurWord2, Dictionary<char, int> occurWord1)
+        {
+            foreach (char c in occurWord1.Keys)
+            {
+                if (occurWord2[c] != occurWord1[c])
+                    return false;
+            }
+            return true;
+        }
+
+        private static bool SameKeys(Dictionary<char, int> occurWord2, Dictionary<char, int> occurWord1)
+        {
+            foreach (char c in occurWord1.Keys)
+            {
+                if (!occurWord2.Keys.Contains(c))
+                    return false;
+            }
+            return true;
+        }
+
+        private static void MapOccurrences(string word1, Dictionary<char, int> occurWord1)
+        {
+            foreach (char c in word1)
+            {
+                if (!occurWord1.Keys.Contains(c))
+                    occurWord1.Add(c, word1.Select(x => x == c).Count());
+            }
+        }
+    }
+}
