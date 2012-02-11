110e9cfe37df09c5b3e55acdb2649a3e61ff524f
Antonio <tonyx@yahoo.com>
2012-02-11 23:12:46 +0100

    cleaning features

--- Anagrams.feature
+++ Anagrams.feature
@@ -3,16 +3,13 @@
 	As a math idiot
 	I want to be told the sum of two numbers
 
-#@mytag
 Scenario: An empty dictionary
 	Given I have an empty dictionary  
-#	And I have entered 70 into the calculator
 	When I ask for the anagrams of the word xxx
 	Then the result should be an empy set of angrams
 
 Scenario: Dictionary with a single word
 	Given I have dictionary containing the words: foo   
-#	And I have entered 70 into the calculator
 	When I ask for the anagram of the word foo
 	Then the result should be a set containing the words: foo
 
@@ -29,8 +26,3 @@ Scenario: Dictionary with three words that are no all anagrams
 
 
 
-#Scenario: Dictionary with three words, two are anagrams and one is not
-#	Given I have dictionary containing the words "foo" "oof" "xxx"   
-#	And I have entered 70 into the calculator
-#	When I ask for the anagram of the words "foo"
-#	Then the result should be a set not containing the words "xxx"
--- Anagrams.feature.cs
+++ Anagrams.feature.cs
@@ -70,13 +70,13 @@ namespace AnagramsKata
         public virtual void AnEmptyDictionary()
         {
             TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("An empty dictionary", ((string[])(null)));
-#line 7
+#line 6
 this.ScenarioSetup(scenarioInfo);
-#line 8
+#line 7
  testRunner.Given("I have an empty dictionary");
-#line 10
+#line 8
  testRunner.When("I ask for the anagrams of the word xxx");
-#line 11
+#line 9
  testRunner.Then("the result should be an empy set of angrams");
 #line hidden
             this.ScenarioCleanup();
@@ -87,13 +87,13 @@ this.ScenarioSetup(scenarioInfo);
         public virtual void DictionaryWithASingleWord()
         {
             TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("Dictionary with a single word", ((string[])(null)));
-#line 13
+#line 11
 this.ScenarioSetup(scenarioInfo);
-#line 14
+#line 12
  testRunner.Given("I have dictionary containing the words: foo");
-#line 16
+#line 13
  testRunner.When("I ask for the anagram of the word foo");
-#line 17
+#line 14
  testRunner.Then("the result should be a set containing the words: foo");
 #line hidden
             this.ScenarioCleanup();
@@ -104,13 +104,13 @@ this.ScenarioSetup(scenarioInfo);
         public virtual void DictionaryWithTwoWordsThatAreAnagramsEachOther()
         {
             TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("Dictionary with two words that are anagrams each other", ((string[])(null)));
-#line 20
+#line 17
 this.ScenarioSetup(scenarioInfo);
-#line 21
+#line 18
  testRunner.Given("I have dictionary containing the words: foo, oof");
-#line 22
+#line 19
  testRunner.When("I ask for the anagram of the word foo");
-#line 23
+#line 20
  testRunner.Then("the result should be a set containing the words: foo, oof");
 #line hidden
             this.ScenarioCleanup();
@@ -121,13 +121,13 @@ this.ScenarioSetup(scenarioInfo);
         public virtual void DictionaryWithThreeWordsThatAreNoAllAnagrams()
         {
             TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("Dictionary with three words that are no all anagrams", ((string[])(null)));
-#line 25
+#line 22
 this.ScenarioSetup(scenarioInfo);
-#line 26
+#line 23
  testRunner.Given("I have dictionary containing the words: foo, oof, xxx");
-#line 27
+#line 24
  testRunner.When("I ask for the anagram of the word foo");
-#line 28
+#line 25
  testRunner.Then("the result should not contain the words: xxx");
 #line hidden
             this.ScenarioCleanup();
